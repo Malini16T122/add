f54b792928325a37bee18530fddf692177575f05
Malini16T122 <48021773+Malini16T122@users.noreply.github.com>
2019-02-26 21:21:53 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+  int num;
+  scanf("%d",&num);
+  if(num== 0)
+  {
+     printf("Number : zero");
+  }
+  else if(num>0)
+  {
+    printf("Number :positive");
+  }
+  else if(num<0)
+  {
+    printf("Number: negative");
+  }
+  }
